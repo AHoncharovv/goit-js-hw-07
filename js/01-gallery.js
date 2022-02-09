37e64eebcfef494f14632af232ed850a02afe67a
Anton Honcharov <antonhoncharovv@gmail.com>
2022-02-09 22:13:49 +0300

    рендерит разметку, добавляет слушателя событий

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGalleryMarkup(items) {
+    const markup = items.map(item => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join("");
+    
+    galleryContainer.insertAdjacentHTML("beforeend", markup)
+};
+createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener("click", event => {
+    event.preventDefault();
+    const currentElement = event.target;
+    const currentValue = event.currentTarget;
+    
+
+    if (!currentElement) return;
+     
+    // заменяет ссылку на картинку при клике
+    currentElement.src = currentElement.dataset.source
+
+    console.log(currentElement);
+   
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// const galleryContainer = document.querySelector(".gallery");
+
+// function createGalleryMarkup(items) {
+//     const markup = items.map(item => {
+//         return `
+//         <div class="gallery__item">
+//             <a class="gallery__link" href="large-image.jpg">
+//                 <img
+//                     class="gallery__image"
+//                     src="${item.preview}"
+//                     data-source="${item.original}"
+//                     alt="${item.description}"
+//                 />
+//             </a>
+//         </div>
+//         `;
+//     }).join("");
+    
+//     galleryContainer.insertAdjacentHTML("beforeend", markup)
+// };
+
+// createGalleryMarkup(galleryItems);
